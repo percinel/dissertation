09c431b65a65645e6902faa1a747293acdf32b99
Kasim Necdet Percinel <necdetper@gmail.com>
2014-04-06 02:29:04 +0300

    confirm and get_mail moved to bootstrap

--- Controller/UsersController.php
+++ Controller/UsersController.php
@@ -39,19 +39,20 @@ class UsersController extends AppController {
 				$email_code = Security::hash(rand(1000,10000000).$email.rand(1000,10000000), NULL, true);
 				$message = "You can either follow the <a href='".Router::fullbaseUrl()."/users/confirm/".$email_code."'>link</a><br/>Or you can confirm your email by entering this code $email_code to your ";
 				if($this->sendMail($email,'Email Confirmation Link',$message)) {
-                	$this->Session->setFlash(__('Please check your mail box , we have send a confirmation link.'));
+                	$this->Session->setFlash(__('Please check your mail box , we have send a confirmation link.'),'login_error');
 					$this->User->saveField('email',$email);
 					$this->User->saveField('email_code',$email_code);
 					$this->User->saveField('email_validated',1);
 					$this->Session->write('Auth', $this->User->read(null, $this->Auth->user('id')));
                 	return $this->redirect(array('action' => 'confirm'));
 				}
-                $this->Session->setFlash(__('There is a serious error, please contact with the it help desk'));
+                $this->Session->setFlash(__('There is a serious error, please contact with the it help desk','login_error'));
             } else {
-                $this->Session->setFlash(__('Please enter a valid email account.'));
+                $this->Session->setFlash(__('invalid email'),'login_error');
                 return $this->redirect('get_mail');
 			}
         }
+        $this->Session->setFlash(__('please validate your email'),'login_error');
 	}
 
 	public function confirm($passcode = null) {
@@ -75,7 +76,7 @@ class UsersController extends AppController {
                 return $this->redirect(array('action' => 'home'));
 				
 			} else {
-                $this->Session->setFlash(__('You have a very serious error with your account, please contact with the ithelpdesk'));
+                $this->Session->setFlash(__('You have a very serious error with your account, please contact with the ithelpdesk'),'login_error');
                 return $this->redirect(array('action' => 'logout'));
 			}
         }
--- View/Users/confirm.ctp
+++ View/Users/confirm.ctp
@@ -1,8 +1,25 @@
-<div class="users form">
-<?php echo $this->Form->create('User'); ?>
-    <fieldset>
-        <legend>Please enter the validation code we have send to your email box</legend>
-        <?php echo $this->Form->input('passcode'); ?>
-    </fieldset>
-<?php echo $this->Form->end(__('Submit')); ?>
-</div>
+<?php $this->layout = 'login_pages'; ?>
+<div class="header"><?php echo __('Confirm Your Email'); ?></div>
+<?php echo $this->Form->create('User',
+	array('action'=>'confirm',
+		'inputDefaults'=> array(
+			'label'=>false,
+			'div'=>false
+		)
+	)
+); ?>
+	<div class="body bg-gray">
+		<?php echo $this->Session->flash(); ?>
+		<?php echo $this->Form->input('passcode',array(
+			'type'=>'text',
+			'div'=>'form-group',
+			'placeholder'=>'Confirmation Code',
+			'class'=>'form-control',
+		)); ?>
+	</div>
+	<div class="footer">
+		<button type="submit" class="btn bg-olive btn-block">
+			<?php echo __('Validate Email'); ?>
+		</button>  
+	</div>
+</form>
